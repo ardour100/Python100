d77b0b25e203399d4437a521f84698ccd18a10c6
Ting <ting@Tings-MBP.lan>
2025-06-09 15:15:52 +1000

    Python Runner

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import PythonRunner from './PythonRunner.jsx';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Python 100</h1>
+      <PythonRunner />
+    </>
+  )
+}
+
+export default App
